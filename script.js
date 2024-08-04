afdf16a49570797ba57a3082032316314f358c51
Rebecka C. F. dos Santos <105793789+berecka04@users.noreply.github.com>
2024-08-04 20:01:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,29 +6,29 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Saber o que queremos para nosso futuro é crucial para a escolha da faculdade ideal que vai impulsionar nosso futuro. Vocẽ ja sabe o que quer cursar ou tem uma base do que vai escolher?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Sim. Já me decidi do que quero!",
+                afirmacao: "Esse é um bom começo, estar decidido do que quer cursar é um passo muito importante!"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Não. Ainda tenho dúvidas sobre o que quero!",
+                afirmacao: "Ter dúvidas sobre o que escolher pro futuro faz parte, mas essa  incerteza pode acabar te atrapalhando!"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Caso você ja tenha se decidido do que quer cursar, esse curso é um sonho de criança ou você pretende cursar pensando no retorno benéfico financeiro?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Sim, esse curso é sonhado por anos, é algo que gosto muito. ",
+                afirmacao: "Cursar e trabalhar com o que gosta, nos motiva a querer estar vivos todos os dias."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
+                texto: "Em tempos atuais, é melhor deixar o que gosta de lado e pensar no retorno financeiro.",
+                afirmacao: "São tempos difíceis, e as vezes, largar de mão de um sonho pode significar um bom controle financeiro no futuro."
+            }  
         ]
     },
     {
