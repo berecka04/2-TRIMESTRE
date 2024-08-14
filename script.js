7fef6bf451070101833a37174546756768e48b2c
Rebecka C. F. dos Santos <105793789+berecka04@users.noreply.github.com>
2024-08-14 09:48:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Saber o que queremos para nosso futuro é crucial para a escolha da faculdade ideal que vai impulsionar sua vida. Vocẽ ja sabe o qual profissão quer escolher para seu futuro?",
+        enunciado: "VOCẼ SABE O QUE GOSTA? FAÇA ESSE TESTE E VEJA QUAL SEU TIPO DE PERSONALIDADE",
         alternativas: [
             {
                 texto: "Sim. Já me decidi do que quero!",
-                afirmacao: "Esse é um bom começo, estar decidido do que quer é um passo muito importante!"
+                afirmacao: "."
             },
             {
                 texto: "Não. Ainda tenho dúvidas sobre o que quero!",
-                afirmacao: "Ter dúvidas sobre o que escolher pro futuro faz parte, mas essa  incerteza pode acabar te atrapalhando!"
+                afirmacao: "."
             }
         ]
     },
@@ -23,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, esse curso é sonhado por anos, é algo que gosto muito. ",
-                afirmacao: "Cursar e trabalhar com o que gosta, nos motiva a querer estar vivos todos os dias."
+                afirmacao: "."
             },
             {
                 texto: "Em tempos atuais, é melhor deixar o que gosta de lado e pensar no retorno financeiro.",
-                afirmacao: "São tempos difíceis, e as vezes, largar de mão de um sonho pode significar um bom controle financeiro no futuro."
+                afirmacao: "."
             }  
         ]
     },
@@ -36,11 +36,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Pela falta de incen",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                afirmacao: "."
             },
             {
                 texto: "Não pretendo cursar faculdade, falta de aptidão e/ou prefiro cursos profissionalizantes.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                afirmacao: "."
             }
         ]
     },
@@ -49,11 +49,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                afirmacao: "."
             },
             {
                 texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                afirmacao: "."
             }
         ]
     },
@@ -62,11 +62,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                afirmacao: "."
             },
             {
                 texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                afirmacao: "."
             }
         ]
     },
